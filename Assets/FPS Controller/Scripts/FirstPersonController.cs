1cb8251c1998258000665e056fe1ccf27f414cf0
KitchenGun <kitchengun124@gmail.com>
2020-07-09 12:11:25 +0900

    Initial commit

---
+++ Assets/FPS Controller/Scripts/FirstPersonController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float MouseX { get; private set; }
+    public float MouseY { get; private set; }
+    public bool Grounded { get; private set; }
+
+    private Rigidbody rigidbody;
+    
+    
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();   
+    }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal * 5f, rigidbody.velocity.y, vertical * 5f);
+        rigidbody.velocity = transform.TransformDirection(direction);
+
+        if(Input.GetKey(KeyCode.Space)&&Grounded)
+        {
+            rigidbody.AddForce(transform.up * 5, ForceMode.Impulse);
+        }
+        if(Physics.SphereCast(transform.position,0.3f,Vector3.down,out RaycastHit hitInfo,1.1f))
+        {
+            Grounded = true;
+        }
+        else
+        {
+            Grounded = false;
+        }
+    }
+
+
+    private void Update()
+    {
+        MouseX += Input.GetAxisRaw("Mouse X");
+        MouseY -= Input.GetAxisRaw("Mouse Y");
+
+        //Debug.LogFormat("mouseX{0},mouseY{1}", MouseX, MouseY);
+
+        Vector3 eularAngles = new Vector3(0, MouseX, 0);
+        transform.localEulerAngles = eularAngles;
+
+        Camera mainCamera = Camera.main;
+        mainCamera.transform.localEulerAngles = new Vector3(MouseY, 0, 0);
+
+
+    }
+}
---
+++ Assets/TPS Controller/Scripts/ThirdPersonController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+
+    private float MouseX = 0f;
+    private Vector3 InputVector = Vector3.zero;
+    private float MoveSpeed = 10f;
+    private float YSpeed = 0f;
+    [SerializeField]
+    private CharacterController character;
+    [SerializeField]
+    private Animator animator;
+
+    private void Start()
+    {
+        //character = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        
+
+        //회전
+        MouseX += Input.GetAxisRaw("Mouse X") * 120f * Time.deltaTime;
+        Quaternion q = Quaternion.Euler(0, MouseX, 0);
+        transform.rotation = q;
+        //이동
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        InputVector = new Vector3(horizontal, 0, vertical)*MoveSpeed;
+
+        if(character.isGrounded==false)
+        {
+            YSpeed -= 0.05f;
+        }
+
+        InputVector.y = YSpeed;
+        character.Move(transform.TransformVector(InputVector) * Time.deltaTime);
+
+        animator.SetBool("Grounded", character.isGrounded);
+        animator.SetInteger("Vspeed", (int)InputVector.z);
+
+        if(Input.GetKey(KeyCode.LeftShift)&&0<InputVector.z)
+        {
+            MoveSpeed = 20;
+        }
+        else
+        {
+            MoveSpeed = 5;
+        }
+        //점프
+        if(Input.GetButton("Jump")&&character.isGrounded)
+        {
+            YSpeed = 5f;
+        }
+    }
+}
